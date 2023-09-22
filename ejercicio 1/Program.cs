3c01327085974149303dbadd01f0f9e495fc543d
Juan Paris <juanpajares6@gmail.com>
2023-09-21 20:27:27 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número 1:");
+            int num1=int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el número 2:");
+            int num2=int.Parse(Console.ReadLine());
+            int resultado = 0;
+            for (int i=0;i<num1; i++)
+            {
+                resultado = resultado + num2;
+            }
+            Console.WriteLine("El producto es:" + resultado);
+            Console.ReadKey();
+        }
+    }
+}
